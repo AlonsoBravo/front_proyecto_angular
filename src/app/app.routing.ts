8a174122cc888c27a4c03642007070422f271647
Alonso Bravo <42778327+AlonsoBravo@users.noreply.github.com>
2019-04-25 16:56:32 -0400

    Maquetacin de componentes

---
+++ src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateProjectComponent } from './components/create-project/create-project.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
+
+const appRoutes: Routes = [
+
+	{path: '', component: AboutComponent},
+	{path: 'sobre-mi', component: AboutComponent},
+	{path: 'proyectos', component: ProjectsComponent},
+	{path: 'crear-proyecto', component: CreateProjectComponent},
+	{path: 'contacto', component: ContactComponent},
+	{path: '**', component: ErrorComponent},
+
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
